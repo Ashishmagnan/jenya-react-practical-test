48d31e9d8db3cf80d086c96331d1884521096f3b
Ashish Magnani <ashishmagnani07@gmail.com>
2026-03-28 23:19:24 +0530

    formated task

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import {
-  Container,
-  TextField,
-  Button,
-  Typography,
-  Paper
+    Container,
+    TextField,
+    Button,
+    Typography,
+    Paper
 } from "@mui/material";
 
 import { useDispatch } from "react-redux";
@@ -12,73 +12,73 @@ import { loginUser } from "../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const [form, setForm] = useState({
-    username: "",
-    password: ""
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    dispatch(loginUser(form)).then(() => {
-      navigate("/products");
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
     });
-  };
 
-  return (
-    <Container maxWidth="sm">
-      <Paper
-        elevation={3}
-        sx={{ padding: 4, marginTop: 8 }}
-      >
-        <Typography
-          variant="h4"
-          gutterBottom
-        >
-          Login
-        </Typography>
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        <form onSubmit={handleSubmit}>
-          <TextField
-            fullWidth
-            label="Username"
-            margin="normal"
-            onChange={(e) =>
-              setForm({
-                ...form,
-                username: e.target.value
-              })
-            }
-          />
+        dispatch(loginUser(form)).then(() => {
+            navigate("/products");
+        });
+    };
 
-          <TextField
-            fullWidth
-            label="Password"
-            type="password"
-            margin="normal"
-            onChange={(e) =>
-              setForm({
-                ...form,
-                password: e.target.value
-              })
-            }
-          />
+    return (
+        <Container maxWidth="sm">
+            <Paper
+                elevation={3}
+                sx={{ padding: 4, marginTop: 8 }}
+            >
+                <Typography
+                    variant="h4"
+                    gutterBottom
+                >
+                    Login
+                </Typography>
 
-          <Button
-            variant="contained"
-            fullWidth
-            sx={{ mt: 2 }}
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
-      </Paper>
-    </Container>
-  );
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        fullWidth
+                        label="Username"
+                        margin="normal"
+                        onChange={(e) =>
+                            setForm({
+                                ...form,
+                                username: e.target.value
+                            })
+                        }
+                    />
+
+                    <TextField
+                        fullWidth
+                        label="Password"
+                        type="password"
+                        margin="normal"
+                        onChange={(e) =>
+                            setForm({
+                                ...form,
+                                password: e.target.value
+                            })
+                        }
+                    />
+
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        sx={{ mt: 2 }}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </form>
+            </Paper>
+        </Container>
+    );
 }
 
 export default Login;
--- src/pages/productList.jsx
+++ src/pages/productList.jsx
@@ -4,82 +4,82 @@ import { fetchProducts } from "../features/products/productSlice";
 
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Container,
-  Typography,
-  Button
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Container,
+    Typography,
+    Button
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function ProductList() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const { products } = useSelector(
-    (state) => state.products
-  );
+    const { products } = useSelector(
+        (state) => state.products
+    );
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
 
-  return (
-    <>
-    
+    return (
+        <>
 
-      <Container sx={{ mt: 4 }}>
-       <Button
+
+            <Container sx={{ mt: 4 }}>
+                <Button
                     variant="contained"
                     sx={{ mt: 2 }}
-                    onClick={()=>navigate("/crud")}
-                  >
+                    onClick={() => navigate("/crud")}
+                >
                     CRUD Operation
-                  </Button>
-        <Typography
-          variant="h4"
-          gutterBottom
-        >
-          Product List
-        </Typography>
+                </Button>
+                <Typography
+                    variant="h4"
+                    gutterBottom
+                >
+                    Product List
+                </Typography>
 
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Title</TableCell>
-                <TableCell>Price</TableCell>
-                <TableCell>Brand</TableCell>
-              </TableRow>
-            </TableHead>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Price</TableCell>
+                                <TableCell>Brand</TableCell>
+                            </TableRow>
+                        </TableHead>
 
-            <TableBody>
-              {products.map((item) => (
-                <TableRow key={item.id}>
-                  <TableCell>
-                    {item.title}
-                  </TableCell>
+                        <TableBody>
+                            {products.map((item) => (
+                                <TableRow key={item.id}>
+                                    <TableCell>
+                                        {item.title}
+                                    </TableCell>
 
-                  <TableCell>
-                    ${item.price}
-                  </TableCell>
+                                    <TableCell>
+                                        ${item.price}
+                                    </TableCell>
 
-                  <TableCell>
-                    {item.brand}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
-    </>
-  );
+                                    <TableCell>
+                                        {item.brand}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+        </>
+    );
 }
 
 export default ProductList;
